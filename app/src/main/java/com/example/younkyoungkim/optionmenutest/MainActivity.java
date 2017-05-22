81e04330c82f379be1b6180e5fff36cbc8fc4c19
kimyounkyoung <passposs2@naver.com>
2017-05-22 20:50:07 +0900

    #2 oncreateOptionsmenu overriding

--- app/src/main/java/com/example/younkyoungkim/optionmenutest/MainActivity.java
+++ app/src/main/java/com/example/younkyoungkim/optionmenutest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.younkyoungkim.optionmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+        public boolean onCreateOptionsMenu(Menu menu){
+            super.onCreateOptionsMenu(menu);
+            MenuInflater mInflater = getMenuInflater();
+            mInflater.inflate(R.menu.menu1,menu);
+            return true;
     }
 }
